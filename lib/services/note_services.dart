157d0b5dc4b73c2b7dff3b45f13d4d052fe31b44
isanmh <1157050074@student.uinsgd.ac.id>
2024-05-27 15:15:08 +0700

    User services

---
+++ lib/services/note_services.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:notes_api/models/note_model.dart';
+import 'package:notes_api/utils/constants.dart';
+import 'package:http/http.dart' as http;
+
+class NoteServices {
+  // get
+  Future getNotes() async {
+    try {
+      final res = await http.get(Uri.parse(notesUrl));
+      if (res.statusCode == 200) {
+        Iterable it = jsonDecode(res.body);
+        List<Note> notes = it.map((e) => Note.fromJson(e)).toList();
+        return notes;
+      } else {
+        return "Error";
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // post
+  Future postNote(String title, String content) async {
+    try {
+      final res = await http.post(
+        Uri.parse(notesUrl),
+        body: {
+          "title": title,
+          "content": content,
+        },
+      );
+      if (res.statusCode == 201) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // put
+  Future putNote(int id, String title, String content) async {
+    try {
+      final res = await http.put(
+        Uri.parse('$notesUrl/${id.toString()}'),
+        body: {
+          "title": title,
+          "content": content,
+        },
+      );
+      if (res.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // delete
+  Future deleteNote(int id) async {
+    try {
+      final res = await http.delete(
+        Uri.parse('$notesUrl/${id.toString()}'),
+      );
+      if (res.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -10,6 +10,7 @@ const String postUrl = '$_baseurl/posts';
 const String userUrl = 'https://reqres.in/api/users?page=2';
 
 // https://mockapi.io/
+const String notesUrl = 'https://66543b021c6af63f4676f3c7.mockapi.io/api/notes';
 
 showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
